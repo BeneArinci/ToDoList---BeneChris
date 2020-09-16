88f60b4009f56c67ef8307c66c20392edb86c49d
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-09-16 15:18:37 +0100

    Able to add new Todo to list and render on screen

--- src/ToDoForm/ToDoForm.js
+++ src/ToDoForm/ToDoForm.js
@@ -1,13 +1,38 @@
 import React, { Component } from "react";
+import ToDoList from "../ToDoList/ToDoList";
 
 class ToDoForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todoArray: [],
+      input: "",
+    };
+  }
+
+  updateInput = event => {
+    this.setState({ input: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      todoArray: [...this.state.todoArray, this.state.input],
+      input: "",
+    });
+  };
+
   render() {
+    const { todoArray, input } = this.state;
     return (
-      <form>
-        <label>New Todo</label>
-        <input type="text" />
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <form>
+          <label>New Todo</label>
+          <input type="text" onChange={this.updateInput} value={input} />
+          <input type="submit" value="Submit" onClick={this.handleSubmit} />
+        </form>
+        <ToDoList todoArray={todoArray} />
+      </div>
     );
   }
 }
--- src/ToDoForm/ToDoForm.test.js
+++ src/ToDoForm/ToDoForm.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import ToDoForm from "./ToDoForm";
 
@@ -10,4 +11,14 @@ test("There is a text box and submit button on the page", () => {
 
   expect(textbox).toBeInTheDocument();
   expect(button).toBeInTheDocument();
+});
+
+test("You can submit the form to add a new test", () => {
+  render(<ToDoForm />);
+  const textbox = screen.getByRole("textbox");
+  const button = screen.getByRole("button", { name: /Submit/ });
+  userEvent.type(textbox, "Do washing");
+  userEvent.click(button);
+  const todo = screen.getByText("Do washing");
+  expect(todo).toBeInTheDocument();
 });
